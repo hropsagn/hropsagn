483694024ef13993ca3c32bf4e96882df2fc8e99
hropsagn <49510206+hropsagn@users.noreply.github.com>
2019-04-11 11:48:20 +0530

    Update hunterone1.py

--- hunterone1.py
+++ hunterone1.py
@@ -1,7 +1,7 @@
-nm=int(input())
+n=int(input())
 a = [int(x) for x in input().split()]
-for i in range(0,nm):
-    if i<nm-1:
+for i in range(0,n):
+    if i<n-1:
         k=' '
     else:
         k=''
